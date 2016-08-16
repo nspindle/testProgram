9966110545f58b085e0943c2404ceea2babb21e3
nspindle <nspindle@broncos.uncfsu.edu>
2016-08-16 11:23:02 -0400

    upload testPorgram.py

---
+++ testProgram.py
@@ -0,0 +1,16 @@
+###########################
+# Title: Test Program
+# Version: 1.0
+# Date: today
+# Author: Nate Spindler
+###########################
+'''A test program'''
+def main():
+    '''The Main function / method.  It runs everything.'''
+    user = input('Hello.  Please enter your name. ')
+    print('Hello,',user)
+
+  
+if __name__ == "__main__":
+    main()
+    
